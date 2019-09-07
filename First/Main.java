d1b6072157b04bcfe5af9e4ebc25cfef069a670a
Ravineel Singhi <Ravineel@users.noreply.github.com>
2019-09-07 12:14:30 +0530

    First Java program

---
+++ First/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+/**
+ * Write a description of class Main here.
+ *
+ * @author (Ravineel)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your private 
+
+    public Main()
+    {
+       
+       
+       // initialise instance variables
+       
+    }
+    public static void main(String[] args){
+        String name="India";
+        int population = 12345678;
+        float populationdensity =232.4f;
+        double gdp = 2.5343;
+        char currency='R';
+        boolean issecular= true;
+        if(issecular){
+        System.out.println(name+" "+gdp);
+        }
+        
+        
+    
+    }
+
+ 
+    
+}
